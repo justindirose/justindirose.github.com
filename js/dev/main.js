89650fe539fe9af09734c208a509529965fd1c7e
Justin DiRose <dirose.justin@gmail.com>
2015-11-28 16:11:49 -0600

    Removed slider js

--- js/dev/main.js
+++ js/dev/main.js
@@ -2,11 +2,4 @@
 // MAIN APP JQUERY
 // ****************************************************
 
-
-// NAVIGATION
-// Triggers change on nav-icon from hamburger to X
-$(document).ready(function(){
-    $('label[for="nav-trigger"]').click(function(){
-        $('#nav-icon').toggleClass('open');
-    });
-});
+//
